e1276d7feba4fd837a54475eadf0a5971e06023e
Prince Ryan Sy <prince@workabroad.ph>
2018-07-02 08:22:19 +0800

    removed commons folder - needs to be dynamic

--- common/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('ROOTPATH', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
-define('APPPATH', ROOTPATH . 'application' . DIRECTORY_SEPARATOR);
-define('CONFIGPATH', APPPATH . 'config' . DIRECTORY_SEPARATOR);
-define('LOGPATH', APPPATH . 'logs' . DIRECTORY_SEPARATOR);
-
-define('DB_DEFAULT', 'local');
-define('DB_LOCAL', 'local');
